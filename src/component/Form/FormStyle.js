8a7deccf0ce6aee434ac4d58d52f68edec3c0f2e
arsalannury <arsiiiland@yaho.com>
2021-12-20 16:02:21 +0330

    add Wawe to form and makestyles MUI

---
+++ src/component/Form/FormStyle.js
@@ -0,0 +1,10 @@
+import {makeStyles} from '@mui/styles';
+
+export const useStyles = makeStyles({
+ title_form : {
+     position : 'absolute',
+     top : '0px',
+     left : '10px',
+     fontSize : '1.5em',
+ }
+})
