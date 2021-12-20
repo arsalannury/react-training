f5a7210a7175e482339c694816541cc73866d9ba
arsalannury <arsiiiland@yaho.com>
2021-12-20 14:30:59 +0330

    add STYLED COMPONENT

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import axios from "axios";
+// import {Title} from 'styled-components';
+// import {Wrapper}
+import styled from 'styled-components';
 
+const Title = styled.h1`
+ font-size : 20px;
+ color : #ddd;
+ 
+`
 const Form = () => {
   
   return (
     <>
+    <Title>
+      salam
+    </Title>
     </>
   );
 };
