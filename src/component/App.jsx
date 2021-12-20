dad2f536bb44aaaa788ff8990047eef51c989983
arsalannury <arsiiiland@yaho.com>
2021-12-20 16:24:59 +0330

    Services

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -4,6 +4,7 @@ import "aos/dist/aos.css";
 import Header from './Header/Header';
 import Form from './Form/Form';
 import GalleryAll from './Gallery/GalleryAll';
+import Service from './Services/Service';
 import '../../src/Index.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Header />
     <GalleryAll />
     <Form />
+    <Service />
     </>
   );
 }
