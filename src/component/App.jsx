050576a72740529f07b4a45db3f74348e89600b2
arsalannury <arsiiiland@yaho.com>
2021-12-31 15:50:16 +0330

    restart

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -4,24 +4,11 @@ import "aos/dist/aos.css";
 import "../../src/Index.css";
 // import WaveUp from './Waves/WaveUp';
 // import WaveBottom from './Waves/WaveBottom';
-import Header from "./Header/Header";
-import SliderBox from "./Slider/Slider";
-import RootShoes from "./Introduce/Root";
-import Stepperr from "./Stepper/Stepper";
-import StepperTitle from "./Stepper/StepperTitle";
-import ProviderSale from "./SaleSlider/Provider";
 
 function App() {
   AOS.init();
   return (
     <>
-      <Header />
-      <SliderBox />
-      <RootShoes />
-      <StepperTitle />
-      <Stepperr />
-
-      <ProviderSale />
     </>
   );
 }
--- src/context/ContextShoes.js
+++
@@ -1,4 +0,0 @@
-import {createContext} from 'react';
-
- const ShoesContext = createContext()
- export default ShoesContext;
