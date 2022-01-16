da3ef6adf6cfc9887292491bdc985daed8e2c709
arsalannury <arsiiiland@yaho.com>
2022-01-16 17:22:02 +0330

    add header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -13,9 +13,6 @@ class Header extends Component {
               <MenuItem>ساخت محصول</MenuItem>
             </Menu>
           </ColumnThree>
-          <ColumnTwo>
-            <Search src="search.svg" />
-          </ColumnTwo>
           <ColumnOne>
             <Image src="Sun.png" />
           </ColumnOne>
@@ -27,10 +24,12 @@ class Header extends Component {
 
 export default Header;
 
-const Container = styled.div`
+const Container = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  height : 100px;
+  padding : 10px;
 `;
 const Image = styled.img`
   width: 190px;
@@ -38,10 +37,35 @@ const Image = styled.img`
 const Search = styled.img`
   width: 30px;
 `;
-const ColumnOne = styled.div``;
+const ColumnOne = styled.div`
+ padding-top : 35px;
+`;
 
 const ColumnTwo = styled.div``;
-const ColumnThree = styled.div``;
+const ColumnThree = styled.div`
+display : flex;
+align-items : center;
+justify-content : center;
+`;
 
-const Menu = styled.ul``;
-const MenuItem = styled.li``;
+const Menu = styled.ul`
+display : flex;
+align-items : center;
+justify-content : space-around;
+width : 300px;
+`;
+const MenuItem = styled.li`
+list-style-type : none;
+cursor : pointer;
+font-size : .9em;
+padding : 9px;
+transition : all .5s ease ;
+&:hover{
+  color : #003566;
+}
+`;
+
+
+// <ColumnTwo>
+//             <Search src="search.svg" />
+//           </ColumnTwo>
