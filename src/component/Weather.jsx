f807028d8f34a2419fbede3ada1ff20d8b043db7
arsalannury <arsiiiland@yaho.com>
2022-01-21 23:46:11 +0330

    weather design -1

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -18,7 +18,6 @@ function Weather() {
   const [done, setDone] = useState(true);
   const handleChange = (e) => {
     setCity(e.target.value);
-    if (e.target.value.length === 0) setDone(true);
   };
   const handlePress = async (e) => {
     if (e.key === "Enter") {
@@ -31,6 +30,7 @@ function Weather() {
       getIcon(
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
       );
+      e.target.value = "";
     }
   };
 
@@ -40,18 +40,36 @@ function Weather() {
         <h1 className={classes.title}>Search Weather City</h1>
       ) : (
         <Grid className={classes.grid_container} container>
-          <Grid item>
+          <Grid item className={classes.box_icon}>
             <img src={icon} alt="icon_weather" className={classes.icon} />
           </Grid>
-          <Grid item>
+          <Grid item className={classes.description}>
+            <Typography className={classes.situation}>
+              Situation Weather
+            </Typography>
             <Typography>{currents.weather[0].description}</Typography>
           </Grid>
-          <Grid item>
-            <Typography>{currents.wind.speed}</Typography>
-            <Typography>{currents.wind.deg}</Typography>
+          <Grid item className={classes.wind}>
+            <Box className={classes.box_wind}>
+              <Typography className={classes.title_wind}>Wind Speed</Typography>
+              <Typography>{currents.wind.speed}</Typography>
+            </Box>
+            <Box className={classes.box_wind}>
+              <Typography className={classes.title_wind}>Wind Deg</Typography>
+              <Typography>{currents.wind.deg}</Typography>
+            </Box>
           </Grid>
-          <Typography>{currents.name}</Typography>
-          <Typography>{currents.main.temp}</Typography>
+          <Typography className={classes.city}>{currents.name}</Typography>
+          <Box className={classes.temp_main}>
+            <Box className={classes.box_temp}>
+              <Typography className={classes.title_temp}>Temp</Typography>
+              <Typography>{currents.main.temp}</Typography>
+            </Box>
+            <Box className={classes.box_temp}>
+              <Typography className={classes.title_temp}>Feels like Temp</Typography>
+              <Typography>{currents.main.feels_like}</Typography>
+            </Box>
+          </Box>
         </Grid>
       )}
       <Box className={classes.box_textfield}>
--- src/styles/WeatherStyle.js
+++ src/styles/WeatherStyle.js
@@ -6,17 +6,24 @@ export const useWeatherStyle = makeStyles(() => ({
     margin: "auto !important",
   },
   icon: {
-    width: "120px",
+    width: "35%",
+  },
+  box_icon: {
+    width: "100%",
   },
   grid_container: {
     boxShadow: "0 18px 20px #888",
     padding: "20px",
     margin: "100px",
-    minHeight: "300px",
+    minHeight: "500px",
     width: "450px !important",
     borderRadius: "10px",
     flexDirection: "column !important",
     alignItems: "center",
+    // backdropFilter: 'blur(22px) saturate(99%)',
+    // backgroundColor: 'rgba(255, 255, 255, 0.58)',
+    // borderRadius: '12px',
+    // border: '1px solid rgba(209, 213, 219, 0.3)',
   },
   box_textfield: {
     display: "flex",
@@ -27,5 +34,65 @@ export const useWeatherStyle = makeStyles(() => ({
     fontSize: "3em",
     textAlign: "center",
     margin: "100px",
+   
+    
+    background:
+      "linear-gradient(to right, rgb(29, 78, 216), rgb(30, 64, 175), rgb(17, 24, 39))",
+      WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
   },
+  description : {
+    width: '100%',
+    direction: 'ltr',
+    display: 'flex',
+    flexDirection: 'column !important',
+    justifyContent: 'space-between',
+    padding: '10px',
+  },
+  situation : {
+    width: '115px',
+    borderBottom: '1px solid #ddd',
+},
+box_wind : {
+    display: 'flex',
+    flexDirection: 'column !important',
+    justifyContent: 'space-between',
+    alignItems : 'center',
+    padding: '10px',
+    width : '100%',
+},
+wind : {
+    width: '100%',
+    display: 'flex',
+    direction: 'ltr',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding : '15px',
+},
+title_wind : {
+    borderBottom : '1px solid #ddd',
+},
+city : {
+    fontSize: '3em',
+    padding: '30px',
+},
+temp_main : {
+    width: '100%',
+    display: 'flex',
+    direction: 'ltr',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding : '15px',
+},
+box_temp : {
+    width: '100%',
+    display: 'flex',
+    padding: '10px',
+    alignItems: 'center',
+    flexDirection: 'column !important',
+    justifyContent: 'space-between',
+},
+title_temp : {
+    borderBottom : '1px solid #ddd',
+}
 }));
