eba71a9a856342ce6bb767e5309508228cf9cfca
arsalannury <arsiiiland@yaho.com>
2022-02-19 21:41:52 +0330

    colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
-function Home() {
+function Home(props) {
   return (
     <Main>
-        <Show></Show>
+        <Show style={{background : props.back}}>{props.back}</Show>
       <ColorContainer>
-        <Color></Color>
-        <Color></Color>
-        <Color></Color>
-        <Color></Color>
-        <Color></Color>
+        <Color1 onClick={props.one}></Color1>
+        <Color2 onClick={props.two}></Color2>
+        <Color3 onClick={props.three}></Color3>
+        <Color4 onClick={props.four}></Color4>
+        <Color5 onClick={props.five}></Color5>
       </ColorContainer>
     </Main>
   );
@@ -17,7 +17,47 @@ function Home() {
 
 export default Home;
 
-const Color = styled.span``
-const ColorContainer = styled.div``
-const Show = styled.div``
-const Main = style.div``
+const Color1 = styled.span`
+margin: 0 10px 0 10px;
+background : #fb8500;
+width: 30px;
+height: 30px;
+border-radius: 50%;
+cursor : pointer;
+`
+const Color2 = styled(Color1)`
+background : #ffb703   ;
+`
+const Color3 = styled(Color1)`
+background : #023047   ;
+`
+const Color4 = styled(Color1)`
+background : #219ebc   ;
+`
+const Color5 = styled(Color1)`
+background : #8ecae6   ;
+`
+
+const ColorContainer = styled.div`
+display : flex;
+align-items : center;
+jsutify-content : space-evenly;
+margin : 50px 0 0 0 ;
+`
+const Show = styled.div`
+height : 150px;
+width : 200px;
+border-radius : 10px;
+border : 1px solid #333;
+display : flex;
+align-items : center;
+justify-content : center;
+font-size : 20px;
+color : #fff;
+`
+const Main = styled.div`
+display : flex;
+align-items : center;
+justify-content : space-around;
+flex-direction : column;
+`
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import ReactDom from 'react-dom';
 import {createStore} from 'redux';
-import Home from './src/components/Home';
+import Home from './components/Home';
+import reducer from './components/reducers/reducer';
 
+const store = createStore(reducer)
 
-ReactDom.render(
-    <Home />,
-    document.getElementById('Project')
-)
+const render = () => {
+    ReactDom.render(
+        <Home
+        back={store.getState()}
+        one={() => store.dispatch({type : 'ONE'})}
+        two={() => store.dispatch({type : 'TWO'})}
+        three={() => store.dispatch({type : 'THREE'})}
+        four={() => store.dispatch({type : 'FOUR'})}
+        five={() => store.dispatch({type : 'FIVE'})}
+        />,
+        document.getElementById('Project')
+    )
+}
+
+render()
+
+store.subscribe(render)
