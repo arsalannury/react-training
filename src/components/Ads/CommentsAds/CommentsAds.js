da3f4bdbe81e4ce9d47920536d68551eb1f9d71e
arsalannury <arsiiiland@yaho.com>
2022-02-24 01:29:10 +0330

    comment section

--- src/components/Ads/CommentsAds/CommentsAds.js
+++ src/components/Ads/CommentsAds/CommentsAds.js
@@ -1,29 +1,46 @@
 import { useState, useEffect } from "react";
-import { Grid } from "@mui/material";
+import { Grid, Button } from "@mui/material";
 import styled from "styled-components";
 import axios from "axios";
 import CommentsLoading from "../../Loading/CommentLoading/CommentsLoading";
+import getCommentsApi from "./GetComments";
+import Slide from 'react-reveal/Slide';
 
 const CommentsAds = () => {
-  const [commentOne, getCommentOne] = useState([]);
+  const [comments, getComments] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [more, getMore] = useState(false);
 
   useEffect(() => {
+    getMore(false)
     const getData = async () => {
       const response = await axios.get(
         "https://api.freerealapi.com/comments/618a862db883dbaafa9449c5"
       );
       try {
-        console.log(response.data.comment);
-        getCommentOne(response.data.comment);
+        console.log([response.data.comment]);
+        getComments([response.data.comment]);
         setLoading(false);
       } catch (error) {
-        throw new Error("کاربر مورد نظر وجود ندارد");
         setLoading(true);
+        throw new Error("کاربر مورد نظر وجود ندارد");
       }
     };
     getData();
   }, []);
+
+  const handleButtonMore = () => {
+    getCommentsApi(getComments, comments);
+    getMore(true);
+  };
+
+  const handleButtonLess = () => {
+    getComments(
+      comments.filter((comment) => comment._id === "618a862db883dbaafa9449c5")
+    );
+    getMore(false)
+  };
+
   return (
     <>
       {loading ? (
@@ -32,7 +49,7 @@ const CommentsAds = () => {
         <Container
           container
           justifyContent={"space-between"}
-          alignItems={"center"}
+          alignItems={"flex-start"}
         >
           <Information
             container
@@ -47,45 +64,111 @@ const CommentsAds = () => {
           >
             <H2>نظرات مشتریان انگلیسی زبان</H2>
             <P>میتوانید با یک خرید</P>
-            <P>به جامعه ی بزرگ زود باش متصل شوید</P>
+            <P>
+              به جامعه ی بزرگ <Span>زود باش</Span> متصل شوید
+            </P>
           </Information>
 
-          <Comments item xs={12} sm={12} md={6} lg={6}>
-            <SingleComment
-              container
-              flexDirection={"column"}
-              alignItems={"flex-start"}
-            >
-              <TextComment>{commentOne.text}</TextComment>
-              <Name>{commentOne.user.name}</Name>
-              <Email>{commentOne.user.email}</Email>
-            </SingleComment>
+          <Comments
+            item
+            container
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+            justifyContent={"center"}
+          >
+            {comments.map((cmt, index) => (
+              <Slides top>
+              <SingleComment
+                key={index}
+                container
+                flexDirection={"column"}
+                alignItems={"flex-start"}
+              >
+                <TextComment>{cmt.text}</TextComment>
+                <UserPersonal
+                  container
+                  alignItems={"flex-end"}
+                  justifyContent={"space-evenly"}
+                  flexDirection={"column"}
+                >
+                  <Name>{cmt.user.name}</Name>
+                  <Email>{cmt.user.email}</Email>
+                </UserPersonal>
+              </SingleComment>
+              </Slides>
+            ))}
+
+            {!more ? (
+              <ButtonMore
+                onClick={handleButtonMore}
+                sx={{ fontFamily: "inherit" }}
+                variant="outlined"
+              >
+                موارد بیشتر
+              </ButtonMore>
+            ) : (
+              <ButtonLess
+                variant="outlined"
+                sx={{ fontFamily: "inherit" }}
+                onClick={handleButtonLess}
+              >
+                موارد کمتر
+              </ButtonLess>
+            )}
           </Comments>
         </Container>
       )}
     </>
   );
 };
-
 export default CommentsAds;
 
 const Container = styled(Grid)`
   height: 400px;
+  margin: 10px 0 0 0;
 `;
 const Comments = styled(Grid)``;
-const Comment = styled(Grid)``;
+
+const UserPersonal = styled(Grid)`
+  margin-top: 10px;
+`;
 
 const Information = styled(Grid)``;
-const H2 = styled.h2``;
-const P = styled.p``;
+const H2 = styled.h2`
+margin-top: 10px;
+`;
+const P = styled.p`
+margin-top: 10px;
+`;
+const Span = styled.span`
+  color: #cd1c1c;
+`;
 
 const SingleComment = styled(Grid)`
   direction: ltr;
   box-shadow: 0 0 15px #ccc;
   border-radius: 5px;
-  margin: 0 0 0 10px;
+  margin: 20px 15px 0 15px;
   padding: 10px;
 `;
-const TextComment = styled.p``;
-const Name = styled.p``;
-const Email = styled.p``;
+const Slides = styled(Slide)`
+direction: ltr;
+box-shadow: 0 0 15px #ccc;
+border-radius: 5px;
+margin: 20px 15px 0 15px;
+padding: 10px;
+`
+const TextComment = styled.p`
+  color: #333;
+`;
+const Name = styled.p`
+  font-size: 0.8em;
+  color: #0096c7;
+`;
+const Email = styled(Name)``;
+const ButtonMore = styled(Button)`
+  margin: 30px 0 30px 0;
+`;
+const ButtonLess = styled(ButtonMore)``;
